03eceaab25a8b23aa1b0ac301fc6886216faa4cc
SubodhKatna <subodhkatna@gmail.com>
2025-06-20 10:06:23 +0530

    Done the Problem Best Time To Buy and Sell Stock

---
+++ Easy/Best Time to Buy and Sell Stock/BruteForceApproach.java
@@ -0,0 +1,34 @@
+public class BruteForceApproach {
+    public static void main(String[] args) {
+
+        int[] prices1 = { 7, 1, 5, 3, 6, 4 };
+        System.out.println("Test Case 1: " + maxProfit(prices1)); // Expected: 5
+
+        int[] prices2 = { 7, 6, 4, 3, 1 };
+        System.out.println("Test Case 2: " + maxProfit(prices2)); // Expected: 0
+
+        int[] prices3 = { 1, 2 };
+        System.out.println("Test Case 3: " + maxProfit(prices3)); // Expected: 1
+
+        int[] prices4 = { 2, 4, 1 };
+        System.out.println("Test Case 4: " + maxProfit(prices4)); // Expected: 2
+
+        int[] prices5 = {};
+        System.out.println("Test Case 5: " + maxProfit(prices5)); // Expected: 0
+
+        int[] prices6 = { 3 };
+        System.out.println("Test Case 6: " + maxProfit(prices6)); // Expected: 0
+    }
+
+    private static int maxProfit(int[] prices) {
+        int len = prices.length;
+        int profit = 0;
+        for(int i = 0; i < len; i++){
+            for(int j = i + 1; j < len; j++){
+                int pro = prices[j]-prices[i];
+                profit = Math.max(pro, profit);
+            }
+        }
+        return profit;
+    }
+}
